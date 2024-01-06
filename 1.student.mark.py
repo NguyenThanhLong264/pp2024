6f55b63d050188749509312a70dec6a079c1f254
Nguyen Thanh Long <longnt,22bi13264@usth.edu.vn>
2024-01-06 14:58:09 +0700

    input stu

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,1 +1,11 @@
-print("hello world Usth")
+def process_students(num_students):
+    # Perform some operations based on the number of students
+    if num_students < 0:
+        print("Invalid number of students.")
+    elif num_students == 0:
+        print("There are no students.")
+    else:
+        print(f"There are {num_students} students.")
+
+num_students = int(input("Enter the number of students: "))
+process_students(num_students)
